826e168cd74bc8d1cb0ca19e621e4f587d73f1df
Dan Thal <dthal@mathematica-mpr.com>
2025-02-18 14:04:01 -0500

    Make printing of CART table optional

--- R/make_tree_plot.R
+++ R/make_tree_plot.R
@@ -4,15 +4,18 @@
 #' @param y y
 #' @param cp CART's complexity parameter thingy
 #' @param pfx Prefix for plot
+#' @param print_cp_table whether or not to print out the table of splits
 #' @param ... additional arguments passed to rpart::rpart
 #'
 #' @export
 #'
 
-make_tree_plot <- function (y, x, cp=.1, pfx='y', ...) {
+make_tree_plot <- function (y, x, cp=.1, pfx='y', print_cp_table=FALSE, ...) {
     tree_data <- data.frame(x, yhat = y)
     tree_fit <- rpart::rpart(yhat ~ .,data = tree_data, ...)
-    print(tree_fit$cptable)
+    if (print_cp_table) {
+        print(tree_fit$cptable)
+    }
 
     tree_fit_pruned <- if(is.null(cp)){tree_fit} else {rpart::prune(tree_fit, cp = cp)}
     rpart.plot::prp(tree_fit_pruned, faclen = 0, clip.facs = TRUE,
