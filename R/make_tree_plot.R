700d15c8af22eda2abd8d46e9d5c309f4cff6d9b
Dan Thal <dthal@mathematica-mpr.com>
2025-02-18 13:53:30 -0500

    Fix make_tree_plot dots passthrough

--- R/make_tree_plot.R
+++ R/make_tree_plot.R
@@ -4,13 +4,14 @@
 #' @param y y
 #' @param cp CART's complexity parameter thingy
 #' @param pfx Prefix for plot
+#' @param ... additional arguments passed to rpart::rpart
 #'
 #' @export
 #'
 
 make_tree_plot <- function (y, x, cp=.1, pfx='y', ...) {
     tree_data <- data.frame(x, yhat = y)
-    tree_fit <- rpart::rpart(yhat ~ .,data = tree_data) # Want to determine drivers of cont. propensities
+    tree_fit <- rpart::rpart(yhat ~ .,data = tree_data, ...)
     print(tree_fit$cptable)
 
     tree_fit_pruned <- if(is.null(cp)){tree_fit} else {rpart::prune(tree_fit, cp = cp)}
